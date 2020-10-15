0a40dd0036135edebe8ccba1c7b5ce013132e894
zhengchaohui <1170782807@qq.com>
2020-10-15 14:25:00 +0800

    完善线程池笔记（初步）

--- base/src/com/learn/juc/executor/ThreadPoolDemo.java
+++ base/src/com/learn/juc/executor/ThreadPoolDemo.java
@@ -1,13 +1,18 @@
 package com.learn.juc.executor;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * ThreadPoolDemo
- *
+ * 拒绝策略：当队列满并且线程个数达到maximumPoolSize后采取的策略，
+ * 比如：
+ *  1. AbortPolicy（抛出异常）
+ *  2. CallerRunsPolicy（使用调用者所在线程来运行任务）
+ *  3. DiscardOldestPolicy（调用poll丢弃一个任务，执行当前任务）
+ *  4. DiscardPolicy（默认丢弃，不抛出异常）。
  * @author zhengchaohui
  * @date 2020/10/15 10:45
  */
@@ -17,8 +22,10 @@ public class ThreadPoolDemo {
     private static final int QUEUE_CAPACITY = 100;
     private static final Long KEEP_ALIVE_TIME = 1L;
 
+    private static final int THREAD_NUM = 10;
+
     public static void main(String[] args) {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+        ThreadPoolExecutor executor1 = new ThreadPoolExecutor(
                 CORE_POOL_SIZE,
                 MAX_POOL_SIZE,
                 KEEP_ALIVE_TIME,
@@ -27,18 +34,45 @@ public class ThreadPoolDemo {
                 new ThreadPoolExecutor.CallerRunsPolicy()
         );
 
-        for (int i = 0; i < 10; i++) {
+        ThreadPoolExecutor executor2 = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+
+        List<Future<String>> futureList = new ArrayList<>();
+        Callable<String> callable = new MyCallable();
+
+        for (int i = 0; i < THREAD_NUM; i++) {
             Runnable worker = new MyRunnable(i + "");
-            threadPoolExecutor.execute(worker);
+            executor1.execute(worker);
+
+            Future<String> future = executor2.submit(callable);
+            futureList.add(future);
         }
 
-        threadPoolExecutor.shutdown();
-        while(!threadPoolExecutor.isTerminated()) {}
-        System.out.println("Finished all threads");
+        for (Future<String> future : futureList) {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executor1.shutdown();
+        executor2.shutdown();
+        while(!executor1.isTerminated() || !executor2.isTerminated()) {}
+        System.out.println("executor1 : Finished all threads");
     }
 
 }
 
+/**
+ * Runnable实现
+ */
 class MyRunnable implements Runnable{
 
     private String command;
@@ -67,4 +101,15 @@ class MyRunnable implements Runnable{
         return this.command;
     }
 
+}
+
+/**
+ * Callable实现（有返回值）
+ */
+class MyCallable implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        return "MyCallable:" + Thread.currentThread().getName();
+    }
 }
