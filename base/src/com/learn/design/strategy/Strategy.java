1b0e49281e894c369ef391add02d36df995c84fe
zhengchaohui <1170782807@qq.com>
2020-10-19 15:30:32 +0800

    修改策略模式代码

--- base/src/com/learn/design/strategy/Strategy.java
+++ base/src/com/learn/design/strategy/Strategy.java
@@ -2,11 +2,17 @@ package com.learn.design.strategy;
 
 /**
  * Strategy
- *
+ * 策略类
  * @author zhengchaohui
  * @date 2020/10/19 15:19
  */
 public interface Strategy {
 
-    public int doOperation(int a, int b);
+    /**
+     * 操作方法
+     * @param a 操作数1
+     * @param b 操作数2
+     * @return int
+     */
+    int doOperation(int a, int b);
 }
--- base/src/com/learn/design/strategy/StrategyTest.java
+++ base/src/com/learn/design/strategy/StrategyTest.java
@@ -8,13 +8,16 @@ package com.learn.design.strategy;
  */
 public class StrategyTest {
     public static void main(String[] args) {
-        Context context = new Context(new OperationAdd());
+
+        Context context = new Context();
+
+        context.setStrategy(new OperationAdd());
         System.out.println(context.exec(10, 5));
 
-        Context context2 = new Context(new OperationSubtract());
-        System.out.println(context2.exec(10, 5));
+        context.setStrategy(new OperationSubtract());
+        System.out.println(context.exec(10, 5));
 
-        Context context3 = new Context(new OperationMultiply());
-        System.out.println(context3.exec(10, 5));
+        context.setStrategy(new OperationMultiply());
+        System.out.println(context.exec(10, 5));
     }
 }
