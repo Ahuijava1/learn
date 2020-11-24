3b6bb71b4bb7c37ba390b39d52b3246103a1e9c0
zhengchaohui <1170782807@qq.com>
2020-11-24 17:56:41 +0800

    优化最大回撤率算法

--- base/src/com/learn/base/RateOfReturnDemo.java
+++ base/src/com/learn/base/RateOfReturnDemo.java
@@ -15,7 +15,7 @@ public class RateOfReturnDemo {
 
     private static List<BigDecimal> rateOfReturn = new ArrayList<>();
     private static Random rand = new Random(new Random().nextLong());
-    private static Integer DATA_SIZE = 150;
+    private static Integer DATA_SIZE = 250;
     private static int o = 0, d = 0;
 
     /**
@@ -26,7 +26,7 @@ public class RateOfReturnDemo {
         rand.doubles(DATA_SIZE).limit(DATA_SIZE).forEach(value ->
                 {
                     // 6位小数，向下取整
-                    BigDecimal rate = new BigDecimal(value).setScale(6, 1);
+                    BigDecimal rate = new BigDecimal(value).setScale(12, 1);
                     rateOfReturn.add(rate);
                 });
 
@@ -37,9 +37,9 @@ public class RateOfReturnDemo {
 //        rateOfReturn.add(new BigDecimal("0.50"));
 //        rateOfReturn.add(new BigDecimal("0.46"));
 //        rateOfReturn.add(new BigDecimal("0.44"));
-//        rateOfReturn.add(new BigDecimal("0.42"));
+//        rateOfReturn.add(new BigDecimal("-1"));
 //        rateOfReturn.add(new BigDecimal("0.44"));
-//        rateOfReturn.add(new BigDecimal("0.42"));
+//        rateOfReturn.add(new BigDecimal("-1"));
 //        rateOfReturn.add(new BigDecimal("0.54"));
 //        rateOfReturn.add(new BigDecimal("0.51"));
 //        rateOfReturn.add(new BigDecimal("0.44"));
@@ -95,7 +95,7 @@ public class RateOfReturnDemo {
             }
             for (int j = rateOfReturn.size() - 1; j >= i ; j--) {
                 // 如果右指针比左指针的值大，continue
-                if (rateOfReturn.get(j).compareTo(rateOfReturn.get(i)) >= 0) {
+                if (rateOfReturn.get(j).compareTo(rateOfReturn.get(i)) > 0) {
                     continue;
                 }
 
@@ -112,7 +112,7 @@ public class RateOfReturnDemo {
         d = dest;
         System.out.println("origin, pos: " + origin + ", value: " + rateOfReturn.get(origin)
                 + ", dest, pos: " + dest + ", value: " + rateOfReturn.get(dest)
-                + ", 最大回撤率:" + (1.0 - (rateOfReturn.get(dest).doubleValue() / (rateOfReturn.get(origin)).doubleValue())) + "%");
+                + ", 最大回撤率:" + ((rateOfReturn.get(dest).subtract(rateOfReturn.get(origin))) + "%"));
         long end = System.currentTimeMillis();
         System.out.println((end - start) + "ms");
     }
@@ -183,21 +183,71 @@ public class RateOfReturnDemo {
 
     }
 
+    /**
+     * O(n)
+     */
+    private static void help3() {
+        long start = System.currentTimeMillis();
+        BigDecimal max = new BigDecimal(Integer.MIN_VALUE);
+        BigDecimal min = new BigDecimal(Integer.MAX_VALUE);
+        BigDecimal payback = new BigDecimal(Integer.MIN_VALUE);
+        BigDecimal maxReal = BigDecimal.ZERO;
+        BigDecimal minReal = BigDecimal.ZERO;
+        int maxIndex = 0, minIndex = 0;
+        int maxRealIndex = 0, minRealIndex = 0;
+        int index = 0;
+        for (BigDecimal rate : rateOfReturn) {
+            if (max.compareTo(rate) <= 0)
+            {
+                max = rate;
+                maxIndex = index;
+                min = rate;
+                minIndex = index;
+            }
+            if (min.compareTo(rate) > 0)
+            {
+                min = rate;
+                minIndex = index;
+            }
+
+            if ((max.subtract(min)).compareTo(payback) > 0
+                    && max.compareTo(min) != 0)
+            {
+                maxReal = max;
+                maxRealIndex = maxIndex;
+                minReal = min;
+                minRealIndex = minIndex;
+                payback = max.subtract(min);
+            }
+            index++;
+        }
+
+        System.out.println("最大回测：" + payback);
+        System.out.println("最大值：<pos, value>" + maxRealIndex + ", " + maxReal.toString());
+        System.out.println("最小值：<pos, value>" + minRealIndex + ", " + minReal.toString());
+
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + "ms");
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("数据初始化....");
         init();
         System.out.println("数据初始化完成....");
-        System.out.println("-------------暴力算法：-------------");
-        violence();
+//        System.out.println("-------------暴力算法：-------------");
+//        violence();
         // 优化1
         System.out.println("--------------优化1：--------------");
-        help();
+//        help();
         // 优化2
         System.out.println("--------------优化2：--------------");
 //        help2();
         // 绘图
+        System.out.println("--------------优化2：--------------");
+        help3();
 //        new DrawDemo().go(rateOfReturn, o, d);
-        Map<String, String> map = new HashMap<>(2);
-        System.out.println(map.get(2));
+
     }
 }
