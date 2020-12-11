87823b55282460ab1a208cea2ddc9cda2525ba60
zhengchaohui <1170782807@qq.com>
2020-12-11 18:07:14 +0800

    webmagic将数据保存到json文件

--- spider/src/main/java/com/spider/webmagic/WebMagicProcessor.java
+++ spider/src/main/java/com/spider/webmagic/WebMagicProcessor.java
@@ -3,7 +3,9 @@ package com.spider.webmagic;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
 
 import java.util.List;
 
@@ -29,7 +31,10 @@ public class WebMagicProcessor implements PageProcessor {
         System.out.println("开始爬取...");
         startTime = System.currentTimeMillis();
         // 创建爬虫，设置站点地址为：https://www.cnblogs.com/，并启动5个线程
-        Spider.create(new WebMagicProcessor()).addUrl("https://www.chinadaily.com.cn/china/governmentandpolicy/").thread(5).run();
+        Spider.create(new WebMagicProcessor())
+                .addUrl("https://www.chinadaily.com.cn/china/governmentandpolicy/")
+                .addPipeline(new JsonFilePipeline("D:\\temp\\"))
+                .thread(5).run();
         endTime = System.currentTimeMillis();
         System.out.println("爬取结束，耗时约" + ((endTime - startTime) / 1000) + "秒，抓取了"+count+"条记录");
     }
@@ -40,18 +45,16 @@ public class WebMagicProcessor implements PageProcessor {
      */
     public void process(Page page) {
         // 加入满足条件的链接
+        Html html = page.getHtml();
         page.addTargetRequests(
-                page.getHtml().xpath("//*span[@class=\"tw3_01_2_t\"]/h4/a/@href").all());
+                html.xpath("//*span[@class=\"tw3_01_2_t\"]/h4/a/@href").all());
         //获取页面需要的内容
-        System.out.println("标题："+
-                page.getHtml().xpath("//div[@class=\"main_art\"]/div[@class=\"lft_art\"]/h1/text()").get()
-        );
-        System.out.println("图片1：https:" + page.getHtml().xpath("//div[@id=\"content\"]/figure/img/@src").get());
-        List<String> contents = page.getHtml().xpath("//div[@id=\"content\"]/p/text()").all();
-        System.out.println("---内容---" );
-        for (String content : contents) {
-            System.out.println("    " + content);
-        }
+        String header = html.xpath("//div[@class=\"main_art\"]/div[@class=\"lft_art\"]/h1/text()").get();
+        page.putField("标题", header);
+        List<String> pics = html.xpath("//div[@id=\"content\"]/figure/img/@src").all();
+        page.putField("图片", pics);
+        List<String> contents = html.xpath("//div[@id=\"content\"]/p/text()").all();
+        page.putField("内容", contents);
         count ++;
     }
 
